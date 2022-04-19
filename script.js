a14d24674f26ccf1ce94155ec09dee4e5933850a
YanRonconi <103211493+YanRonconi@users.noreply.github.com>
2022-04-19 11:32:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+alert("Hola mundo")
+
+let str1 = "String con comillas dobles"
+let str2 = 'String con comillas simples'
+let str3 = `String con comillas dobles`
+
+
+let str5 = `${str1} y ademas ${str2} `
+let str6 = str1 + " y ademas " + str2;
+
+
+
+let numerito = 2
+let num2 = 3.14
+let resultado= num2*numerito
+
+console.log(numerito + " y además " + num2)
+
+console.log(numerito * num2)
+console.log(resultado)
